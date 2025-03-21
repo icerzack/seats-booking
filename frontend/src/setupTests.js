1768563097a32f916ce84e5173633827dbf45798
kuznetsov.m <kuznetsov.m@selectel.ru>
2025-03-21 03:49:27 +0300

    fixfixfix

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+// This suppresses React 18 act() warnings
+// Only use this if you can't fix your tests properly
+const originalError = console.error;
+console.error = (...args) => {
+    if (/Warning.*not wrapped in act/.test(args[0])) {
+        return;
+    }
+    originalError.call(console, ...args);
+};
